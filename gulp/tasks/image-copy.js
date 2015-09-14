8952bcccde1d46d9a632e2425cde183eb3719987
馬道 允崇 <umamichi@team-lab.com>
2015-09-14 19:30:04 +0900

    image-copy.jsのパスを修正

--- gulp/tasks/image-copy.js
+++ gulp/tasks/image-copy.js
@@ -3,9 +3,9 @@ var $ = require('gulp-load-plugins')();
 var C = require('../config');
 var changed = require('gulp-changed');
 
-// src/copy -> dest へコピーする
+// src/img -> dest へコピーする
 gulp.task('imgCopy', function () {
     return gulp.src(C.src.imgCopy)
-    .pipe(changed(C.dest.imgCopy))  // 変更ファイルのみコピー
-    .pipe(gulp.dest(C.dest.imgCopy));
+    .pipe(changed(C.dest.img))  // 変更ファイルのみコピー
+    .pipe(gulp.dest(C.dest.img));
 });
